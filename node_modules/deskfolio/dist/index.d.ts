eba7ed3e213765241fd049d0e7cfb7c242a449cb
Rishab Kumar <ezrishab@gmail.com>
2026-08-02 20:47:33 +0530

    Deploy birthday wish

--- node_modules/deskfolio/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactElement } from 'react'
-
-/** The DeskFolio portfolio page (book, desk scene, stickers, scribble underlines). */
-export declare function DeskFolioPage(): ReactElement
-
-/** DeskFolioPage wrapped in the page-level layout classes it expects. */
-export declare function DeskFolio(): ReactElement
